b19a744449b5130e242932a72909460b6dd6fa56
Budi Tanrim <1830184+buditanrim@users.noreply.github.com>
2024-08-27 11:58:30 +0700

    Initial Commit

---
+++ swift-library/README.md
@@ -0,0 +1,6 @@
+#  swift-library
+
+A collection of explanation with examples for Swift
+
+### Swift Data 
+
---
+++ swift-library/swift_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_libraryApp.swift
+//  swift-library
+//
+//  Created by Budi Tanrim on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
